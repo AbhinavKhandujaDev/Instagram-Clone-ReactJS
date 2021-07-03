35ae4c90d434548291a730ab00496512d6dbe4e0
khanduja <abhinavkhanduja@mindztechnology.com>
2021-07-03 23:20:31 +0530

    login/signup ui completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { memo } from "react";
+import "./App.css";
+import Login from "./pages/login/Login";
+import Home from "./pages/Home";
+import SignUp from "./pages/login/SignUp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+var configure = {
+  apiKey: process.env.REACT_APP_FIREBASEAPIKEY,
+  authDomain: process.env.REACT_APP_FIREBASEAUTHDOMAIN,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  projectId: process.env.REACT_APP_FIREBASEPROJECTID,
+};
+
+firebase.initializeApp(configure);
+window.firebase = firebase;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default memo(App);
