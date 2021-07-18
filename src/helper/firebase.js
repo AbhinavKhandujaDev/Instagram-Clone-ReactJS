ff3b6d19ffaa3bd883615c8263f5ade8ffa00bcb
khanduja <abhinavkhanduja@mindztechnology.com>
2021-07-18 16:14:40 +0530

    profile header firebase data set

---
+++ src/helper/firebase.js
@@ -0,0 +1,30 @@
+export async function fetchUserData(key) {
+  if (!key) return;
+  let path = window.usersRef;
+  let resp = await path.child(key).once("value");
+  return resp;
+}
+
+export async function getIdByUsername(username) {
+  if (!username) return;
+  let path = window.usernamesRef;
+  let resp = await path.child(username).once("value");
+  return resp;
+}
+
+export async function getFollowersCount(key, isFollowing = false) {
+  if (!key) return;
+  const ref = isFollowing ? window.userFollowingRef : window.userFollowerRef;
+  let resp = await ref.child(key).once("value");
+  return resp.numChildren();
+}
+export async function getCollectionCount(key, ref) {
+  if (!key || !ref) return;
+  let resp = await ref.child(key).once("value");
+  return resp.numChildren();
+}
+export async function getPosts(key) {
+  let ref = window.userPostsRef;
+  let resp = await ref.child(key).once("value");
+  return resp;
+}
